405031a45ab32e7a6d8609b1f5d3655a08514f60
Kuumoneko <maingocnhat2007@gmail.com>
2025-08-18 15:08:59 +0700

    add duration to local file

--- src/server/pages/home/music/local/index.tsx
+++ src/server/pages/home/music/local/index.tsx
@@ -7,17 +7,18 @@ export default function Local() {
 
     const check = JSON.parse(localStorage.getItem("localfile") as string);
     const [localfile, setlocalfile] = useState(check ? check : []);
+    const [duration, setduration] = useState(0)
     useEffect(() => {
         async function run() {
             const data = await fetch_data(Data.local);
             setlocalfile(data.tracks);
+            setduration(data.duration as number)
         }
         run();
     }, [])
-
     return (
         <>
-            <Top name="local file" thumbnail={null} duration={0} releaseDate="" artists={[]} source="local" id="" mode="playlist" playlist={localfile} />
+            <Top name="local file" thumbnail={null} duration={duration} releaseDate="" artists={[]} source="local" id="" mode="playlist" playlist={localfile} />
             <List list={localfile} source="local" id="local" mode="local" />
         </>
 
