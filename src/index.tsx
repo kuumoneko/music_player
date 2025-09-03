b1ebd4ca48a6e06c7df9f76d643baeee101e5158
Kuumoneko <maingocnhat2007@gmail.com>
2025-09-03 14:43:51 +0700

    fix local storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 // @ts-nocheck
-import Server from './server/index.tsx';
-import Test from './test/server.tsx';
-// import { running } from './prerun.tsx';
-// running();
+import Server from "./server/index.tsx";
+import Test from "./test/server.tsx";
+import { running } from "./prerun.tsx";
+running();
 
-import React, { useEffect, useState } from 'react';
-import { Server_mode } from "./types/index.ts"
-import { createRoot } from 'react-dom/client';
+import React, { useEffect, useState } from "react";
+import { Server_mode } from "./types/index.ts";
+import { createRoot } from "react-dom/client";
 
 const root_element = document.getElementById("root");
 
@@ -15,23 +15,8 @@ const root = createRoot(root_element as HTMLElement);
 const mode: Server_mode = Server_mode.server;
 
 try {
-    root.render(
-        <>
-            {
-                mode === Server_mode.server ? (
-                    <Server />
-                ) : (
-                    <Test />
-                )
-            }
-        </>
-    )
-}
-catch (e) {
-    console.log(e)
-    root.render(
-        <div>
-            {e}
-        </div>
-    )
+    root.render(<>{mode === Server_mode.server ? <Server /> : <Test />}</>);
+} catch (e) {
+    console.log(e);
+    root.render(<div>{e}</div>);
 }
