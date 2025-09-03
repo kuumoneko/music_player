5a213ca097083f48703120dd6c7c9dea5d95c6b7
Kuumoneko <maingocnhat2007@gmail.com>
2025-09-03 10:06:53 +0700

    Fix: track is not defined

--- src/server/pages/home/music/track/index.tsx
+++ src/server/pages/home/music/track/index.tsx
@@ -5,31 +5,48 @@ import List from "../../../../common/components/Show_music/components/list.tsx";
 import Loading from "../../../../common/components/Loading/index.tsx";
 
 export default function Track({ url }: { url: string }) {
-
     const [dom, setdom] = useState(<Loading mode="Loading track" />);
 
     const refresh = async (source: string, id: string) => {
-        const dataa = await fetch_data(Data.track, { where: source, id: id })
+        const dataa = await fetch_data(Data.track, { where: source, id: id });
         setdom(
             <>
-                <Top name={dataa?.track.name} thumbnail={dataa?.thumbnail} duration={dataa?.track.duration} releaseDate={dataa?.track.releaseDate} artists={dataa?.artists} source={source} id={id} mode={"track"} />
-                <List list={dataa !== null ? [
-                    {
-                        thumbnail: dataa?.thumbnail,
-                        track: dataa?.track,
-                        artists: dataa?.artists,
-                        name: dataa?.track.name,
-                        duration: dataa?.track.duration,
-                        releaseDate: dataa?.track.releaseDate
+                <Top
+                    name={dataa[0]?.track.name}
+                    thumbnail={dataa[0]?.thumbnail}
+                    duration={dataa[0]?.track.duration}
+                    releaseDate={dataa[0]?.track.releaseDate}
+                    artists={dataa[0]?.artists}
+                    source={source}
+                    id={id}
+                    mode={"track"}
+                />
+                <List
+                    list={
+                        dataa[0] !== null
+                            ? [
+                                  {
+                                      thumbnail: dataa[0]?.thumbnail,
+                                      track: dataa[0]?.track,
+                                      artists: dataa[0]?.artists,
+                                      name: dataa[0]?.track.name,
+                                      duration: dataa[0]?.track.duration,
+                                      releaseDate: dataa[0]?.track.releaseDate,
+                                  },
+                              ]
+                            : []
                     }
-                ] : []} source={source} id={id} mode="track" />
+                    source={source}
+                    id={id}
+                    mode="track"
+                />
             </>
-        )
-    }
+        );
+    };
 
     useEffect(() => {
         const [source, id] = url.split("/").slice(2);
-        refresh(source, id)
-    }, [url])
-    return dom
+        refresh(source, id);
+    }, [url]);
+    return dom;
 }
