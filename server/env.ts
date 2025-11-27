c4eccaaa0ea07f7c2de67a7e337452d3c658ee1a
Kuumoneko <maingocnhat2007@gmail.com>
2025-11-27 08:43:03 +0700

    check env before running

---
+++ server/env.ts
@@ -0,0 +1,27 @@
+import { existsSync, mkdirSync, writeFileSync } from "node:fs";
+import { resolve } from "node:path"
+
+const all_paths = [
+    { path: '/data/local/local.json', default: '{}' },
+    { path: '/data/system.json', default: '{}' },
+    { path: '/data/profile.json', default: '{}' },
+    // { path: '/data/new_tracks.json', default: '[]' },
+    { path: '/data/spotify/tracks.json', default: '{}' },
+    { path: '/data/spotify/playlists.json', default: '{}' },
+    { path: '/data/youtube/playlists.json', default: '{}' },
+    { path: '/data/youtube/tracks.json', default: '{}' },
+    { path: '/data/youtube/artists.json', default: '{}' }
+]
+
+export default function check_env(executableDir: string) {
+    all_paths.forEach((path: { path: string, default: string }) => {
+        const file = resolve(executableDir, ...path.path.split("/"));
+        if (!existsSync(file)) {
+            mkdirSync(file, { recursive: true });
+            writeFileSync(file, path.default, { encoding: "utf-8" });
+        }
+        else {
+            console.log(`${file} already exists`)
+        }
+    })
+}
