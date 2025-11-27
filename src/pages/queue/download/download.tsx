b26a2f43f68247c803a7f7dcf1173f775a398e47
Kuumoneko <maingocnhat2007@gmail.com>
2025-11-27 08:48:00 +0700

    update Queue Pages

--- src/pages/queue/download/download.tsx
+++ src/pages/queue/download/download.tsx
@@ -1,7 +1,8 @@
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
-import { Data, fetch_data } from "../../../../utils/fetch.ts";
+// import { Data, fetch_data } from "../../../../common/utils/fetch.ts";
+import fetch from "@/utils/fetch.ts";
 
 enum Status {
     idle = "idle",
@@ -9,7 +10,7 @@ enum Status {
     done = "done",
 }
 
-export default function Download({ list }: { list: any[] }) {
+export default function Download() {
     const [download, setdownload] = useState<boolean>(false);
     const [status, setstatus] = useState<Status>(Status.idle);
     const [data, setdata] = useState<string>("");
@@ -19,45 +20,33 @@ export default function Download({ list }: { list: any[] }) {
             try {
                 async function get_download_status() {
                     if (!status) return;
-                    const data: { status: { data: string, track: string } } = await fetch_data(Data.download_status);
-
-                    if (data.status.data == Status.done) {
+                    const { data, track } = await fetch(
+                        "/download/status",
+                        "GET"
+                    );
+                    if (data == Status.done) {
                         setstatus(Status.done);
                         setdownload(false);
-                        setdata("")
-                    }
-                    else if (data.status.data == Status.downloading) {
+                        setdata("");
+                    } else if (data == Status.downloading) {
                         setstatus(Status.downloading);
-                    }
-                    else if (data.status.data === Status.idle) {
+                    } else if (data === Status.idle) {
                         setstatus(Status.idle);
-                    }
-                    else {
-                        setdata(data.status.data + " " + data.status.track);
+                    } else {
+                        setdata(data + " " + track);
                     }
                 }
                 if (status !== "done") {
                     get_download_status();
                 }
-            } catch { }
+            } catch {}
         }, 200);
         return () => window.clearInterval(intervalId);
     }, []);
 
     useEffect(() => {
         async function run() {
-            const links = list.map((item: any) => {
-                return {
-                    source: item.source,
-                    mode: item.mode,
-                    id: item.id,
-                };
-            });
-            const format = localStorage.getItem("preferredAudioFormat");
-            const data = await fetch_data(Data.download, {
-                format: format,
-                links: links,
-            });
+            const data = await fetch("/download");
             if (data.message == "OK") {
                 setstatus(Status.downloading);
             }
@@ -74,28 +63,22 @@ export default function Download({ list }: { list: any[] }) {
                 onClick={() => {
                     if (status === Status.idle) {
                         setdownload(true);
-                    }
-                    else {
-                        setdata("Now you cant click this")
+                    } else {
+                        setdata("Now you cant click this");
                     }
                 }}
             >
-                <span className="bg-slate-50 h-9 w-9 rounded-[50px] ml-[6px] flex flex-row items-center justify-center">
+                <span className="bg-slate-50 h-9 w-9 rounded-[50px] ml-1.5 flex flex-row items-center justify-center">
                     <FontAwesomeIcon
                         icon={faDownload}
                         className="text-slate-700 size-4"
                     />
                 </span>
-                <span className="mr-[15px] flex flex-col items-center justify-center mt-[2px] text-lg text-slate-600 transition-all">
+                <span className="mr-[15px] flex flex-col items-center justify-center mt-0.5 text-lg text-slate-600 transition-all">
                     Download
                 </span>
-
             </span>
-            <span>
-                {
-                    data
-                }
-            </span>
+            <span>{data}</span>
         </div>
     );
 }
---
+++ src/pages/queue/play/index.tsx
@@ -0,0 +1,337 @@
+import { useEffect, useState } from "react";
+import { formatDuration } from "@/utils/format.ts";
+import {
+    faShare,
+    faListDots,
+    faDownload,
+    faMinus,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { add_items } from "@/utils/add_items.ts";
+import add_to_download from "@/utils/add_download.ts";
+import localstorage from "@/utils/localStorage.ts";
+
+export default function Play_Queue() {
+    const [queue, setqueue] = useState([]);
+    const [nextfrom, setnextfrom] = useState({ from: "", tracks: [] });
+
+    useEffect(() => {
+        const run = setInterval(async () => {
+            const temp_play_queue = localstorage("get", "play", []);
+            const temp_nextfrom = localstorage("get", "nextfrom", {});
+            setnextfrom(temp_nextfrom);
+            setqueue(temp_play_queue);
+        }, 500);
+        return () => clearInterval(run);
+    });
+
+    return (
+        <>
+            <div
+                onClick={async () => {
+                    localstorage("set", "play", []);
+                }}
+            >
+                <FontAwesomeIcon icon={faMinus} />
+                Clear queue
+            </div>
+            <div
+                onClick={async () => {
+                    localstorage("set", "nextfrom", {
+                        from: "",
+                        tracks: [],
+                    });
+                }}
+            >
+                <FontAwesomeIcon icon={faMinus} />
+                Clear next from
+            </div>
+            <div className="liked_songs flex flex-col max-h-max w-full overflow-y-scroll [&::-webkit-scrollbar]:hidden">
+                {queue.length > 0 && (
+                    <>
+                        {queue.map((item: any, index: number) => {
+                            return (
+                                <div
+                                    key={item.name}
+                                    className={`queue ${
+                                        index + 1
+                                    } flex h-[100px] w-full flex-row items-center justify-between mb-5 bg-slate-700 hover:bg-slate-600`}
+                                    onDoubleClick={async () => {
+                                        localstorage("set", "playing", {
+                                            name: item.name,
+                                            artists: item.artists,
+                                            thumbnail: item.thumbnail,
+                                            source: item.source,
+                                            id: item.id,
+                                            duration: item.duration,
+                                        });
+                                        localstorage("set", "time", "0");
+
+                                        const trackk = queue.findIndex(
+                                            (itemm: any) => {
+                                                return itemm.id === item.id;
+                                            }
+                                        );
+
+                                        setqueue(queue.slice(trackk + 1, -1));
+                                        localstorage(
+                                            "set",
+                                            "play",
+                                            queue.slice(trackk + 1, -1)
+                                        );
+                                    }}
+                                >
+                                    <div className="flex flex-row items-center ml-2.5">
+                                        <span className="thumbnail cursor-default select-none">
+                                            <img
+                                                src={item.thumbnail}
+                                                alt=""
+                                                height={100}
+                                                width={100}
+                                            />
+                                        </span>
+                                        <div className="flex flex-col ml-2.5">
+                                            <span className="title cursor-default select-none">
+                                                {item.name}
+                                            </span>
+                                            <span className="artists cursor-default select-none">
+                                                {item.artists}
+                                            </span>
+                                            <div className="flex flex-row items-center">
+                                                <span className="releaseDate cursor-default select-none">
+                                                    {item.releaseDate}
+                                                </span>
+                                                <span className="duration cursor-default select-none ml-[15px]">
+                                                    {formatDuration(
+                                                        (item.duration as number) /
+                                                            1000
+                                                    )}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="action_button flex flex-row-reverse mr-2.5">
+                                        <span
+                                            className="mr-2.5"
+                                            onClick={() => {
+                                                if (item.source === "youtube") {
+                                                    const url =
+                                                        "https://www.youtube.com/watch?v=" +
+                                                        item.id;
+                                                    navigator.clipboard.writeText(
+                                                        url
+                                                    );
+                                                } else {
+                                                    const url =
+                                                        "https://open.spotify.com/track/" +
+                                                        item.id;
+                                                    navigator.clipboard.writeText(
+                                                        url
+                                                    );
+                                                }
+                                            }}
+                                        >
+                                            <FontAwesomeIcon icon={faShare} />
+                                        </span>
+                                        <span
+                                            className="mr-2.5"
+                                            onClick={async () => {
+                                                const queue = localstorage(
+                                                    "get",
+                                                    "play",
+                                                    []
+                                                );
+                                                queue.push({
+                                                    name: item.name,
+                                                    artists: item.artists,
+                                                    thumbnail: item.thumbnail,
+                                                    source: item.source,
+                                                    id: item.id,
+                                                    duration: item.duration,
+                                                    time: 0,
+                                                });
+                                                localstorage(
+                                                    "set",
+                                                    "play",
+                                                    queue
+                                                );
+                                            }}
+                                        >
+                                            <FontAwesomeIcon
+                                                icon={faListDots}
+                                            />
+                                        </span>
+                                        <span
+                                            className="mr-2.5"
+                                            onClick={() => {
+                                                add_to_download(
+                                                    item.source,
+                                                    "track",
+                                                    item.id,
+                                                    item.name
+                                                );
+                                            }}
+                                        >
+                                            <FontAwesomeIcon
+                                                icon={faDownload}
+                                            />
+                                        </span>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </>
+                )}
+                {nextfrom?.from !== "" && (
+                    <>
+                        {nextfrom?.tracks?.map((item: any, index: number) => {
+                            return (
+                                <div
+                                    key={item.name}
+                                    className={`nextfrom ${
+                                        index + 1
+                                    } flex h-[100px] w-full flex-row items-center justify-between mb-5 bg-slate-700 hover:bg-slate-600`}
+                                    onDoubleClick={() => {
+                                        localstorage("set", "playing", {
+                                            name: item.name,
+                                            artists: item.artists,
+                                            thumbnail: item.thumbnail,
+                                            source: item.source,
+                                            id: item.id,
+                                            duration: item.duration,
+                                        });
+                                        localstorage("set", "time", "0");
+
+                                        const trackk =
+                                            nextfrom.tracks.findIndex(
+                                                (itemm: any) => {
+                                                    return itemm.id === item.id;
+                                                }
+                                            );
+
+                                        add_items(
+                                            item.source,
+                                            nextfrom.from.split(":")[1],
+                                            nextfrom.from.split(":")[2],
+                                            nextfrom.tracks.slice(
+                                                trackk + 1,
+                                                -1
+                                            )
+                                        );
+
+                                        setnextfrom({
+                                            from: nextfrom.from,
+                                            tracks: nextfrom.tracks.slice(
+                                                trackk + 1,
+                                                -1
+                                            ),
+                                        });
+                                    }}
+                                >
+                                    <div className="flex flex-row items-center ml-2.5">
+                                        <span className="thumbnail cursor-default select-none">
+                                            <img
+                                                src={item.thumbnail}
+                                                alt=""
+                                                height={100}
+                                                width={100}
+                                            />
+                                        </span>
+                                        <div className="flex flex-col ml-2.5">
+                                            <span className="title cursor-default select-none">
+                                                {item.name}
+                                            </span>
+                                            <span className="artists cursor-default select-none">
+                                                {item.artists}
+                                            </span>
+                                            <div className="flex flex-row items-center">
+                                                <span className="releaseDate cursor-default select-none">
+                                                    {item.releaseDate}
+                                                </span>
+                                                <span className="duration cursor-default select-none ml-[15px]">
+                                                    {formatDuration(
+                                                        (item.duration as number) /
+                                                            1000
+                                                    )}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="action_button flex flex-row-reverse mr-2.5">
+                                        <span
+                                            className="mr-2.5"
+                                            onClick={() => {
+                                                if (item.source === "youtube") {
+                                                    const url =
+                                                        "https://www.youtube.com/watch?v=" +
+                                                        item.id;
+                                                    navigator.clipboard.writeText(
+                                                        url
+                                                    );
+                                                } else {
+                                                    const url =
+                                                        "https://open.spotify.com/track/" +
+                                                        item.id;
+                                                    navigator.clipboard.writeText(
+                                                        url
+                                                    );
+                                                }
+                                            }}
+                                        >
+                                            <FontAwesomeIcon icon={faShare} />
+                                        </span>
+                                        <span
+                                            className="mr-2.5"
+                                            onClick={async () => {
+                                                const queue = localstorage(
+                                                    "get",
+                                                    "play",
+                                                    []
+                                                );
+                                                queue.push({
+                                                    name: item.name,
+                                                    artists: item.artists,
+                                                    thumbnail: item.thumbnail,
+                                                    source: item.source,
+                                                    id: item.id,
+                                                    duration: item.duration,
+                                                    time: 0,
+                                                });
+                                                localstorage(
+                                                    "set",
+                                                    "play",
+                                                    queue
+                                                );
+                                            }}
+                                        >
+                                            <FontAwesomeIcon
+                                                icon={faListDots}
+                                            />
+                                        </span>
+                                        <span
+                                            className="mr-2.5"
+                                            onClick={() => {
+                                                add_to_download(
+                                                    item.source,
+                                                    "track",
+                                                    item.id,
+                                                    item.name
+                                                );
+                                            }}
+                                        >
+                                            <FontAwesomeIcon
+                                                icon={faDownload}
+                                            />
+                                        </span>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </>
+                )}
+            </div>
+        </>
+    );
+}
