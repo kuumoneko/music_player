2adb73e12181f8d18f3577de963cef50e51ff7d4
Kuumoneko <maingocnhat2007@gmail.com>
2025-11-27 08:45:13 +0700

    update Navigator

---
+++ src/components/Navigator/Controls/index.tsx
@@ -0,0 +1,90 @@
+import localstorage from "@/utils/localStorage.ts";
+import { goto, Force } from "@/utils/url.ts";
+import {
+    faArrowLeft,
+    faArrowsRotate,
+    faArrowRight,
+    faDownload,
+    faList,
+    faDatabase,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+
+export default function ControlPanel() {
+    const [backward, setbackward] = useState(
+        localstorage("get", "backward", [])
+    );
+    const [forward, setforward] = useState(
+        localstorage("get", "forward", [])
+    );
+
+    useEffect(() => {
+        const run = setInterval(() => {
+            setbackward(localstorage("get", "backward", []));
+            setforward(localstorage("get", "forward", []));
+        }, 100);
+        return () => clearInterval(run);
+    }, []);
+
+    return (
+        <div className="controlpanel flex flex-row gap-2.5">
+            <span
+                className={`material-icons flex items-center gap-[5px] text-white cursor-default select-none ${
+                    backward.length === 0
+                        ? "opacity-50 pointer-events-none"
+                        : ""
+                }`}
+                onClick={() => {
+                    const temp = backward.pop() ?? "/";
+                    goto(temp, Force.Backward);
+                }}
+            >
+                <FontAwesomeIcon icon={faArrowLeft} />
+            </span>
+            <span
+                className={`material-icons flex items-center gap-[5px] text-white cursor-default select-none`}
+                onClick={() => {
+                    window.location.reload();
+                }}
+            >
+                <FontAwesomeIcon icon={faArrowsRotate} />
+            </span>
+            <span
+                className={`material-icons flex items-center gap-[5px] text-white cursor-default select-none ${
+                    forward.length === 0 ? "opacity-50 pointer-events-none" : ""
+                }`}
+                onClick={() => {
+                    const temp = forward.shift() ?? "/";
+                    goto(temp, Force.Forward);
+                }}
+            >
+                <FontAwesomeIcon icon={faArrowRight} />
+            </span>
+            <span
+                className={`material-icons flex items-center gap-[5px] text-white cursor-default select-none`}
+                onClick={() => {
+                    goto("/local");
+                }}
+            >
+                <FontAwesomeIcon icon={faDatabase} />
+            </span>
+            <span
+                className="material-icons flex items-center gap-[5px] text-white cursor-default select-none"
+                onClick={() => {
+                    goto("/queue/play");
+                }}
+            >
+                <FontAwesomeIcon icon={faList} />
+            </span>
+            <span
+                className="material-icons flex items-center gap-[5px] text-white cursor-default select-none"
+                onClick={() => {
+                    goto("/queue/download");
+                }}
+            >
+                <FontAwesomeIcon icon={faDownload} />
+            </span>
+        </div>
+    );
+}
--- src/components/Navigator/Theme/index.tsx
+++ src/components/Navigator/Theme/index.tsx
@@ -1,30 +1,32 @@
+import localstorage from "@/utils/localStorage.ts";
+import { goto } from "@/utils/url.ts";
 import { faSun, faMoon, faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { goto } from "../../../../utils/url.ts";
+import { useEffect, useState } from "react";
 
-export default function Themes(
-    {
-        theme,
-        settheme,
-        seturl
-    }:
-        {
-            theme: string,
-            settheme: (a: string) => void,
-            seturl: (a: string) => void
+export default function Settings() {
+    const [theme, setTheme] = useState(localstorage("get", "theme", "dark"));
+    useEffect(() => {
+        if (theme === "dark") {
+            document.documentElement.setAttribute("data-theme", "dark");
+            localstorage("set", "theme", "dark");
+        } else {
+            document.documentElement.setAttribute("data-theme", "light");
+            localstorage("set", "theme", "light");
         }
-) {
-
+    }, [theme]);
     return (
-        <div className="header-icons flex top-[20px] right-[20px] gap-[15px] items-center cursor-pointer">
+        <div className="header-icons flex top-5 right-5 gap-[15px] items-center cursor-pointer">
             <span className="user-mode h-1/2">
                 <div className="flex justify-center items-center space-x-4">
                     <label className="relative inline-block w-[60px] h-8">
                         <input
                             type="checkbox"
                             className="opacity-0 w-0 h-0 peer"
-                            checked={theme === 'light'}
-                            onChange={() => settheme(theme === 'dark' ? 'light' : 'dark')}
+                            checked={theme === "light"}
+                            onChange={() =>
+                                setTheme(theme === "dark" ? "light" : "dark")
+                            }
                         />
                         <span className="slider round absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-gray-300 transition-all duration-400 rounded-[34px] before:absolute before:content-[''] before:h-6 before:w-6 before:left-1 before:bottom-1 before:bg-white before:transition-all before:duration-400 before:rounded-full peer-checked:bg-blue-500 peer-focus:ring-2 peer-focus:ring-blue-500 peer-checked:before:translate-x-6 flex items-center justify-between px-2">
                             <span>
@@ -37,14 +39,9 @@ export default function Themes(
                     </label>
                 </div>
             </span>
-            <span
-                className="material-icons caret-slate-50"
-                onClick={() => {
-                    goto("/settings", seturl);
-                }}
-            >
+            <span className="settings" onClick={() => goto("/settings")}>
                 <FontAwesomeIcon icon={faGear} />
             </span>
         </div>
-    )
+    );
 }
