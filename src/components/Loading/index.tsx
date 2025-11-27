d2bdd0517cf722a3593b9c4142a7aa9e0cb87f54
Kuumoneko <maingocnhat2007@gmail.com>
2025-11-27 08:45:32 +0700

    update Loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,9 +1,10 @@
 export default function Loading({ mode }: { mode: string }) {
     return (
         <div className="flex flex-col items-center justify-center py-4">
-            <div className="spinner border-4 border-gray-200 w-9 h-9 rounded-full border-l-blue-400 animate-spin mx-auto my-4 dark:border-l-blue-300"></div>
-            <p className="text-lg text-gray-700 dark:text-gray-300 mt-2">{mode} , please wait...</p>
+            <div className="spinner border-4 border-gray-200 w-9 h-9 rounded-full  animate-spin mx-auto my-4 border-l-blue-300"></div>
+            <p className="text-lg text-gray-300 mt-2">
+                Loading {mode} , please wait...
+            </p>
         </div>
-
-    )
+    );
 }
