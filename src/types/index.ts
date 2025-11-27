cac624553d1f4f03fe7cc496f7c2b79130558377
Kuumoneko <maingocnhat2007@gmail.com>
2025-11-27 08:44:16 +0700

    remove and add new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,117 +1,91 @@
-export enum Mode {
-    react = "testreact",
-    app = "testapp",
-    deploy = "deploy"
-}
-
-export enum Status {
-    idle = "idle",
-    downloading = "downloading",
-    done = "done",
-    env = "env",
-    prepare = "prepare"
-}
-
-export interface Download_queue {
-    link: string,
-    formatlink?: string,
-    title: string,
-    mode: string,
-    format: string,
-    from: string
-}
-
-export enum Audio_format {
-    aac = "aac",
-    alac = "alac",
-    flac = "flac",
-    m4a = "m4a",
-    mp3 = "mp3",
-    opus = "opus",
-    vorbis = "vorbis",
-    wav = "wav"
-}
-
 export interface Artist {
-    type?: string | "spotify:artist" | "youtube:artist",
-    tracks?: Track[],
+    etag?: string,
     name: string,
-    thumbnail?: string,
-    id?: string,
-    error?: string,
-    pagetoken?: string,
-    playlistId?: string
-}
-
-export interface User_Artist {
-    type?: string | "spotify:artist" | "youtube:artist",
-    artists?: Artist[],
-    error?: string
-}
-
-export interface Track {
-    type: string | "spotify:track" | "youtube:track",
-    thumbnail?: string | "",
-    artists?: Artist[],
-    track?: {
-        name: string,
-        id: string,
-        duration: number | string, // in miliseconds
-        releaseDate: string,
-    },
-    error?: string
-};
-
-export interface Album {
-    type: string | "spotify:album",
-    name?: string,
-    id?: string,
-    duration?: number | string, // in miliseconds = sum of duration of tracks
-    releaseDate?: string,
-    thumbnail?: string,
-    artists?: Artist[],
-    tracks?: Track[],
-    error?: string
+    id: string,
+    source: "youtube" | "spotify" | "local",
+    tracks: Track[],
+    thumbnail: string,
+    playlistId: string
 }
 
 export interface Playlist {
-    type: string | "spotify:playlist" | "youtube:playlist",
-    name?: string,
-    id?: string,
-    duration?: number | string, // in miliseconds = sum of duration of tracks
-    thumbnail?: string,
+    etag?: string,
+    name: string,
+    id: string,
+    source: "youtube" | "spotify" | "local",
     tracks?: Track[],
-    error?: string,
-    pagetoken?: string
+    ids?: string[],
+    thumbnail: string,
+    duration: number
 }
 
-interface PlaylistItem {
-    playlistName: string,
-    playlistId: string,
-    authorName: string,
-    thumbnail: string
+export interface Album {
+    etag?: string,
+    name: string,
+    id: string,
+    source: "youtube" | "spotify" | "local",
+    tracks: Track[],
+    thumbnail: string,
+    duration: number,
+    releaseDate: string,
+    artists: { id: string, name: string }[]
 }
 
-export interface UserPlaylist {
-    type: string | "spotify:playlist" | "youtube:playlist",
-    playlists?: PlaylistItem[],
-    error?: string
+export interface Track {
+    etag?: string,
+    name: string,
+    id: string,
+    artist: { id: string, name: string }[],
+    source: "youtube" | "spotify" | "local",
+    thumbnail: string,
+    duration: number,
+    releasedDate: string // DD-MM-YYYY,
+    matched?: string | null
 }
 
 export interface Search {
-    type: string | "spotify:search" | "youtube:search",
-    tracks?: Track[],
-    error?: string
+    query: string,
+    source: string,
+    type: "video" | "playlist" | "artist",
+    tracks: Track[],
+    playlists: Playlist[],
+    artists: Artist[]
 }
 
-export interface User {
-    type: string | "spotify:user" | "youtube:user",
-    name?: string,
-    thumbnail?: string,
-    error?: string
+
+export interface Playing_track {
+    name: string,
+    duration: number,
+    id: string,
+    source: "youtube" | "spotify" | "local",
+    thumbnail: string
 }
 
-export const enum Server_mode {
-    server = "server",
-    test = "test"
+export interface PlayingUrl {
+    id: string,
+    url: string,
+    source: "youtube" | "spotify" | "local"
+}
+
+export interface Api_key {
+    key: string,
+    client_id?: string,
+    isReached: boolean
+    when: number
+}
+
+export interface PlayerOption {
+    youtube_api_keys: Api_key[],
+    spotify_api_keys: Api_key[]
+}
+
+export enum sleep_types {
+    no = "nosleep",
+    five = "after 5 minutes",
+    ten = "after 10 minutes",
+    tenfive = "after 15 minutes",
+    threeten = "after 30 minutes",
+    fourfive = "after 45 minutes",
+    hour = "after 1 hours",
+    eot = "end of this track"
 }
