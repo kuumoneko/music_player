77063292cf5584e2291b188ca9849bc2b984fbef
Kuumoneko <maingocnhat2007@gmail.com>
2026-01-04 17:12:21 +0700

    add types to window to update RPC

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,6 +4,13 @@ declare global {
     interface Window {
         electronAPI: {
             api: (mode: string, data: any) => Promise<{ ok: boolean, data: any }>;
-        };
+            close: () => void,
+            minimize: () => void,
+            maximize: () => void
+        },
+        discord: {
+            setmusic: (track: Track) => Promise<{ ok: boolean }>;
+            clearmusic: () => Promise<{ ok: boolean }>;
+        }
     }
 }
