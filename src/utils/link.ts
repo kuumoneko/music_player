d62659acad58c776f13eb8a810f925dcc0d130dc
Kuumoneko <maingocnhat2007@gmail.com>
2026-01-21 21:58:13 +0700

    Fix: convert wrong link

--- src/utils/link.ts
+++ src/utils/link.ts
@@ -5,19 +5,19 @@ export default function convert_link(link: string) {
         // short form
         let temp: string = "";
         if (youtube_link.includes("youtu.be")) {
-            temp = youtube_link.split("youtu.be/")[1];
+            temp = youtube_link.split("youtu.be/")[1].split("&")[0];
         }
         // long form
         else if (youtube_link.includes("youtube.com")) {
             if (youtube_link.includes("watch")) {
-                temp = youtube_link.split("watch?v=")[1]
+                temp = youtube_link.split("watch?v=")[1].split("&")[0]
             }
             else if (youtube_link.includes("list")) {
-                temp = youtube_link.split("?list=")[1]
+                temp = youtube_link.split("?list=")[1].split("&")[0]
             }
         }
         else if (youtube_link.includes("@")) {
-            temp = youtube_link.split("@")[1]
+            temp = youtube_link.split("@")[1].split("&")[0]
             return {
                 source: "youtube",
                 mode: "artists",
