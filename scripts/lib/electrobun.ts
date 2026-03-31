4e619674ff03ab0701bcd514ca7b62415bc0b772
Kuumoneko <maingocnhat2007@gmail.com>
2026-03-31 08:35:42 +0700

    Add building electrobun function

---
+++ scripts/lib/electrobun.ts
@@ -0,0 +1,10 @@
+export default async function Build_Electrobun() {
+
+    console.info("\nBuilding zig electrobun...")
+    Bun.spawnSync(["zig", "build", "-Doptimize=ReleaseSmall"], { cwd: "electrobun/package/src/extractor", stdout: "inherit" })
+    console.info("Done.")
+
+    console.info("\nBuilding local electrobun...")
+    Bun.spawnSync(["bun", "./build.ts"], { cwd: "electrobun/package", stdout: "inherit" })
+    console.info("Done.")
+}
