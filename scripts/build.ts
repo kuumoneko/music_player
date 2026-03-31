71a891fe1805c4a8fa3867f75f3913ca78856555
Kuumoneko <maingocnhat2007@gmail.com>
2026-03-31 08:36:19 +0700

    Modify build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,85 +1,40 @@
 import { resolve } from "node:path";
 import { config } from "dotenv";
+import chose from "./lib/chose";
+import Build_Electrobun from "./lib/electrobun";
+import system from "./lib/system";
+import ElectroBunConfig from "./lib/config"
 config()
 
 console.info("Building vite...");
 try {
-    Bun.spawnSync({
-        cmd: ["vite", "build"],
-    });
+    Bun.spawnSync({ cmd: ["vite", "build"], stdout: "inherit" });
 } catch (error) {
     console.error(error);
     process.exit(0);
 }
 console.info("Done.");
 
+console.log("\nUse ↑/↓ to select, Enter to confirm:\n");
+const isBuildElectrobun = await chose("Do you want to build Local Electrobun first:", ["yes", "no"]);
 
-const thisWorkSpace = import.meta.url.split("file:///")[1];
-const systemFile = Bun.file(
-    resolve(thisWorkSpace, "..", "..", "data", "system.json"),
-);
-const system = await systemFile.json();
-system.isLocal = true;
-system.isDiscord = true;
+if (isBuildElectrobun) {
+    await Build_Electrobun()
+}
 
-await Bun.write(
-    resolve(thisWorkSpace, "..", "..", "data", "tempsystem.json"),
-    JSON.stringify(system)
-);
+const thisWorkSpace = resolve(import.meta.url.split("file:///")[1], "..", "..");
+await system(thisWorkSpace, true, true, "tempsystem")
+const electrobunConfigText = await ElectroBunConfig(thisWorkSpace, false)
 
-let DiscordClientId = process.env["CLIENT_ID"];
-
-const electrobunConfigText = await Bun.file(resolve(thisWorkSpace, "..", "..", "electrobun.config.ts")).text();
-
-const objectString = electrobunConfigText
-    .split("export default ")[1].split(" satisfies ElectrobunConfig")[0]
-    .trim();
-
-const electrobunConfig = (new Function(`return ${objectString}`))();
-electrobunConfig.build.copy["temp.env"] = ".env";
-
-const allFiles = [
-    ...new Bun.Glob("*").scanSync(
-        resolve(thisWorkSpace, "..", "..", "dist", "assets"),
-    ),
-];
-
-const player = allFiles.filter(
-    (item) => item.includes("player") || item.includes("type"),
-);
-const mainUI = allFiles.filter(
-    (item) => !player.includes(item) || item.includes("type"),
-);
-
-player.forEach((item) => {
-    electrobunConfig.build.copy[`dist/assets/${item}`] = `views/assets/${item}`;
-});
-
-mainUI.forEach((item) => {
-    electrobunConfig.build.copy[`./dist/assets/${item}`] =
-        `views/src/assets/${item}`;
-});
-
-await Bun.write(
-    resolve(thisWorkSpace, "..", "..", "temp.env"),
-    `CLIENT_ID=${DiscordClientId}`
-)
-
-electrobunConfig.build.copy["data/tempsystem.json"] = "data/system.json";
-const tempConfig = `import type { ElectrobunConfig } from "electrobun";\n\nexport default ${JSON.stringify(electrobunConfig, null, 2)} satisfies ElectrobunConfig`
-
-console.info("\nRewriting Electrobun config...");
-await Bun.write(resolve(thisWorkSpace, "..", "..", "electrobun.config.ts"), tempConfig);
-console.info("Done.");
 
 console.info("\nBuilding...")
 Bun.spawnSync(["bunx", "electrobun", "build", "--env=stable"], { stdout: "inherit" })
 console.info("Done.")
 
 console.info("Deleting temporary files...")
-await Bun.write(resolve(thisWorkSpace, "..", "..", "electrobun.config.ts"), electrobunConfigText)
-await Bun.file(resolve(thisWorkSpace, "..", "..", "temp.env")).unlink()
-await Bun.file(resolve(thisWorkSpace, "..", "..", "data", "tempsystem.json")).unlink()
+await Bun.write(resolve(thisWorkSpace, "electrobun.config.ts"), electrobunConfigText)
+await Bun.file(resolve(thisWorkSpace, "temp.env")).unlink()
+await Bun.file(resolve(thisWorkSpace, "data", "tempsystem.json")).unlink()
 console.info("Done.")
 
 
