ec7d4ad4730663fa88463cb8ff4f2b3a3e0984dd
Kuumoneko <maingocnhat2007@gmail.com>
2026-04-01 09:44:16 +0700

    Add stderr to debug

--- scripts/lib/electrobun.ts
+++ scripts/lib/electrobun.ts
@@ -1,5 +1,5 @@
 export default async function Build_Electrobun() {
     console.info("\nBuilding local electrobun...")
-    Bun.spawnSync(["bun", "./build.ts", "--release"], { cwd: "electrobun/package", stdout: "inherit" })
+    Bun.spawnSync(["bun", "./build.ts", "--release"], { cwd: "electrobun/package", stdout: "inherit", stderr: "inherit" })
     console.info("Done.")
 }
