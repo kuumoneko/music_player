70c530d556f73113626f8b4139a6513028f985d1
Kuumoneko <maingocnhat2007@gmail.com>
2026-04-07 16:38:29 +0700

    Features: add colecting live video

--- src/mainview/utils/link.ts
+++ src/mainview/utils/link.ts
@@ -9,11 +9,14 @@ export default function convert_link(link: string) {
         }
         // long form
         else if (youtube_link.includes("youtube.com")) {
-            if (youtube_link.includes("watch")) {
+            if (youtube_link.includes("watch?=")) {
                 temp = youtube_link.split("watch?v=")[1].split("&")[0]
             }
-            else if (youtube_link.includes("list")) {
+            else if (youtube_link.includes("?list=")) {
                 temp = youtube_link.split("?list=")[1].split("&")[0]
+            }
+            else if (youtube_link.includes("/live/")) {
+                temp = youtube_link.split("/live/")[1].split("&")[0]
             }
         }
         else if (youtube_link.includes("@")) {
