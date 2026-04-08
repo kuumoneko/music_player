66a2c4173a98e106f28d69513e65874f5b4372ca
Kuumoneko <maingocnhat2007@gmail.com>
2026-04-08 16:11:04 +0700

    Remove player from play controller

--- src/bun/controllers/play.ts
+++ src/bun/controllers/play.ts
@@ -1,11 +1,7 @@
 import { UserData } from "@/shared/types.ts";
-import wait_for_downloader from "../lib/player";
-import Player from "../music/index.ts";
 import { join } from "node:path";
 
-export default async function PlayController(player: Player, data: UserData): Promise<ArrayBuffer> {
-    await wait_for_downloader(player);
-    const folder = player.download_folder;
+export default async function PlayController(folder:string, data: UserData): Promise<ArrayBuffer> {
     const file = Bun.file(join(folder, data.currentPlaying.id));
     return await file.arrayBuffer();
 }
