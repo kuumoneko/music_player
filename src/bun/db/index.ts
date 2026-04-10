739df8d6d342ce6dbbd2ab4555920188281d2e24
Kuumoneko <maingocnhat2007@gmail.com>
2026-04-10 08:18:51 +0700

    Add new function

--- src/bun/db/index.ts
+++ src/bun/db/index.ts
@@ -1,11 +1,16 @@
-import { getPlaylist } from "./playlists/get.ts";
-import setupSQLite from "./setup";
-import { getTracks } from "./tracks/get.ts"
-import { writeManyTracks } from "./tracks/write.ts";
-import { writePlaylist } from './playlists/write';
-import { getLocalFileById, getLocalFileByIndex, getAllLocalFiles } from './local/get';
-import { writeManyLocalFiles } from './local/write';
-import { getArtistById } from './artists/get';
-import writeArtist from './artists/write';
+import getPlaylist from "./playlists/get.ts";
+import setupSQLite from "./setup.ts";
+import getTracks from "./tracks/get.ts";
+import writeTracks from "./tracks/write.ts";
+import writePlaylist from './playlists/write.ts';
+import { getLocalFileById, getLocalFileByIndex, getAllLocalFiles } from './local/get.ts';
+import writeLocalFiles from './local/write.ts';
+import getArtistById from './artists/get.ts';
+import writeArtist from './artists/write.ts';
+import getUserData from './user/get.ts';
+import writeUserData from './user/write.ts';
+import { getUserDatas } from './user/get';
+import { writeUserDatas } from './user/write';
+import { getTrackByName } from './tracks/get';
 
-export { setupSQLite, getTracks, writeManyTracks, getPlaylist, writePlaylist, getLocalFileById, getLocalFileByIndex, getAllLocalFiles, writeManyLocalFiles, getArtistById, writeArtist }
+export { setupSQLite, getTracks, writeTracks, getPlaylist, writePlaylist, getLocalFileById, getLocalFileByIndex, getAllLocalFiles, writeLocalFiles, getArtistById, writeArtist, getUserData, writeUserData, getUserDatas, writeUserDatas,getTrackByName }
