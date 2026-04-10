ccb2e8222767277312c07f01390e0309859969a6
Kuumoneko <maingocnhat2007@gmail.com>
2026-04-10 08:19:49 +0700

    change from userProfile to userData

--- src/mainview/components/Show/common/download.ts
+++ src/mainview/components/Show/common/download.ts
@@ -1,15 +1,10 @@
 import { Track } from "@/shared/types.ts";
 
-export default async function download(item: Track, source: string) {
-    const queue = await window.api.rpc.request.getProfileData("download");
-    queue.push({
-        id: item.id,
-        name: item.name,
-        source: source,
-        mode: "track"
-    })
-    await window.api.rpc.request.setProfileData({
-        key: "download",
+export default async function download(item: Track) {
+    const queue = await window.api.rpc.request.getUserData("downloadQueue");
+    queue.push(item.id)
+    await window.api.rpc.request.setUserData({
+        key: "downloadQueue",
         data: queue
     })
 }
