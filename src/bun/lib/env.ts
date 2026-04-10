9403911530692c9c31b7d2acc874c5c75c316998
Kuumoneko <maingocnhat2007@gmail.com>
2026-04-10 08:18:42 +0700

    Remove old checking userdata Now checking in sqlite

--- src/bun/lib/env.ts
+++ src/bun/lib/env.ts
@@ -1,75 +1,44 @@
-import { resolve } from 'node:path';
-import consolelog, { LogType } from "../lib/log.ts"
-import { rm, readdir, unlink, mkdir, exists } from 'node:fs/promises';
+import { Repeat, Shuffle, UserData } from '../../shared/types.ts';
+import { getUserData, writeUserData } from '../db/index.ts';
 
-export async function checkUserDataFolder(userDataFolder: string) {
-    const all_paths = [
-        { path: "log.json", default: "[]" },
-        { path: 'local.json', default: '[]' },
-        { path: 'profile.json', default: '{"play":[],"local":[],"folder":"","pin":[],"download":[]}' },
-        { path: 'tracks.json', default: '{}' },
-        { path: 'playlists.json', default: '{}' },
-        { path: 'artists.json', default: '{}' },
-        { path: 'user.json', default: '{"repeat":0,"shuffle":0,"volume":50,"height":600,"width":800,"currentPlaying":{"source":"","id":"","title":"","thumbnail":"","artist":""},"isMaximized":false,"queue":[],"nextfrom":{"from":"","next":[]},"QuitonClose":false,"playedTrack":[],"isLoading":false,"current":{"time":0,"duration":0},"isPlaying":false}' }
+export default function CheckUserData() {
+    const defaultUserData = [
+        { key: "repeat", value: Repeat.Disable },
+        { key: "shuffle", value: Shuffle.Disable },
+        { key: "volume", value: 50 },
+        {
+            key: "currentPlaying",
+            value: {
+                source: "",
+                id: "",
+                title: "",
+                thumbnail: "",
+                artist: "",
+                index: null
+            }
+        },
+        {
+            key: "nextfrom",
+            value: { from: "", next: [] }
+        },
+        { key: "playedTrack", value: [] },
+        { key: "QuitonClose", value: false },
+        {
+            key: "current",
+            value: { time: 0, duration: 0, isLived: false }
+        },
+        { key: "isPlaying", value: false },
+        { key: "isLoading", value: false },
+        { key: "playQueue", value: [] },
+        { key: "folder", value: "" },
+        { key: "pin", value: [] },
+        { key: "downloadQueue", value: [] }
     ]
 
-    async function getAllFilesInDirectory(dir: string, fileArray: string[] = []) {
-        if (!await exists(dir)) return fileArray;
-        const items = await readdir(dir, { withFileTypes: true, recursive: true });
-
-        for (const item of items) {
-            const fullPath = resolve(dir, item.name);
-            if (item.isDirectory()) {
-                const temp = await getAllFilesInDirectory(fullPath, fileArray);
-                fileArray = [...new Set(fileArray), ...temp];
-            } else {
-                fileArray.push(fullPath);
-            }
-        }
-        return fileArray;
-    }
-
-    const dataDir = resolve(userDataFolder, "data");
-    if (!await exists(dataDir)) {
-        await mkdir(dataDir, { recursive: true });
-    }
-    const expectedFiles = all_paths.map(p => ({
-        path: resolve(dataDir, p.path),
-        default: p.default
-    }));
-    const files = await getAllFilesInDirectory(dataDir);
-
-
-    for (const file of expectedFiles) {
-
-        if (!files.includes(file.path)) {
-            await Bun.write(Bun.file(file.path), file.default);
-        }
-    }
-
-    for (const file of files) {
-        if (!expectedFiles.map(p => p.path).includes(file)) {
-            await unlink(file);
-        }
-    }
-
-    //remove all folder
-    const items = await readdir(dataDir, { withFileTypes: true });
-    for (const item of items) {
-        if (item.isDirectory()) {
-            const fullPath = resolve(dataDir, item.name);
-            try {
-                await rm(fullPath, { recursive: true });
-            } catch (e) {
-                consolelog(e, LogType.Error)
-            }
-        }
-    }
-
-    for (const item of expectedFiles) {
-        const data = await Bun.file(item.path).text();
-        if (!data || data.length < item.default.length) {
-            await Bun.write(Bun.file(item.path), item.default)
+    for (const item of defaultUserData) {
+        const checking = getUserData(item.key as keyof UserData);
+        if (checking === null || checking === undefined || typeof item.value !== typeof checking) {
+            writeUserData(item.key as keyof UserData, item.value);
         }
     }
 }
