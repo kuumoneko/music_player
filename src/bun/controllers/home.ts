39c6ff44f1abf086bf87c4d2f23a154a8f89d410
Kuumoneko <maingocnhat2007@gmail.com>
2026-04-10 08:14:01 +0700

    New home get

--- src/bun/controllers/home.ts
+++ src/bun/controllers/home.ts
@@ -1,12 +1,26 @@
 import { Artist } from "@/shared/types.ts";
 import Player from "../music/index.ts";
 
-export default async function HomeController(player: Player, pin: any[]) {
-    const artists = pin.filter((item: any) => item.type.includes("artist"));
-    const playlists = pin.filter((item: any) => item.type.includes("playlist") || item.type.includes("album"));
-    const tracks = pin.filter((item: any) => item.type.includes("track"));
+export default async function HomeController(player: Player, pin: string[]) {
+    const newPins: any[] = pin.map(item => {
+        const [source, type, id] = item.split(":")
+        return {
+            source, type, id
+        }
+    })
+    const artists = newPins.filter((item: any) => item.type.includes("artist"));
+    const pinArtists = [];
+    for (const item of artists) {
+        pinArtists.push(await player.youtube.fetch_artist(item.id))
+    }
+    const playlists = newPins.filter((item: any) => item.type.includes("playlist"));
+    const pinPlaylists = [];
+    for (const item of playlists) {
+        pinPlaylists.push(await player.youtube.fetch_artist(item.id))
+    }
+    const tracks = newPins.filter((item: any) => item.type.includes("track"));
 
     const ytb_new_tracks = await player.youtube.get_new_tracks(artists.filter((item: Artist) => item.source === "youtube").map((item: Artist) => item.id))
 
-    return { artists, playlists, newTracks: ytb_new_tracks, tracks }
+    return { artists: pinArtists, playlists: pinPlaylists, newTracks: ytb_new_tracks, tracks }
 }
