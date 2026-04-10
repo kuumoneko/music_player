d43d88664fdc454de86c55befb0848d0716519d3
Kuumoneko <maingocnhat2007@gmail.com>
2026-04-10 08:15:34 +0700

    using sqlite to get userdata

--- src/bun/controllers/play.ts
+++ src/bun/controllers/play.ts
@@ -1,7 +1,8 @@
-import { UserData } from "@/shared/types.ts";
 import { join } from "node:path";
+import { getUserData } from "../db";
 
-export default async function PlayController(folder:string, data: UserData): Promise<ArrayBuffer> {
-    const file = Bun.file(join(folder, data.currentPlaying.id));
+export default async function PlayController(folder: string): Promise<ArrayBuffer> {
+    const currentPlaying = getUserData("currentPlaying")
+    const file = Bun.file(join(folder, currentPlaying.id));
     return await file.arrayBuffer();
 }
