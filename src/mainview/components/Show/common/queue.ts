67b28a0f04c2ee72c6ccd9052e579bb5608f887c
Kuumo Mai <maingocnhat2007@gmail.com>
2026-04-14 09:26:47 +0700

    Fix: queue need to be string

--- src/mainview/components/Show/common/queue.ts
+++ src/mainview/components/Show/common/queue.ts
@@ -5,7 +5,7 @@ export default async function Queue(item: Track) {
     const removeDuplicates = (list: any[]) => {
         return Array.from(new Set(list));
     };
-    queue.push(item)
+    queue.push(`${item.source}:${item.id}`)
     const temp = removeDuplicates(queue);
     window.api.rpc.request.setUserData({ key: "playQueue", data: temp });
 }
