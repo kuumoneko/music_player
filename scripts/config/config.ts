0d48c9048dd00d407258571fb7ff07ea88b39db3
Kuumo Mai <maingocnhat2007@gmail.com>
2026-04-20 15:55:35 +0700

    Create temporary config

--- scripts/config/config.ts
+++ scripts/config/config.ts
@@ -1,25 +1,38 @@
 import { resolve } from "node:path";
+import { config as envConfig } from "dotenv";
+
+envConfig();
 
 export default async function config(thisWorkSpace: string, isDev: boolean) {
-    const electrobunConfigText = await Bun.file(
-        resolve(thisWorkSpace, "electrobun.config.ts"),
-    ).text();
+    const packageJson = await Bun.file(
+        resolve(thisWorkSpace, "package.json")
+    ).json();
 
-    const objectString = electrobunConfigText
-        .split("export default ")[1]
-        .split(" satisfies ElectrobunConfig")[0]
-        .trim();
+    const electrobunConfig: any = {}
 
-    const electrobunConfig = new Function(`return ${objectString}`)();
+    // default config
+    electrobunConfig.app = {
+        name: packageJson.name,
+        identifier: process.env["IDENTIFIER"] ?? "local.build.dev",
+        version: packageJson.version
+    }
+    electrobunConfig.build = {
+        bunVersion: packageJson.dependencies.bun.split("^")[1],
+        win: {
+            bundleCEF: false,
+            icon: "assets/favicon.ico"
+        },
+        copy: {}
+    }
 
     electrobunConfig.build.copy[`dist/`] = `views/src`;
     electrobunConfig.build.copy[`bin/`] = `bin/`;
+    electrobunConfig.build.copy[`assets/`] = `assets/`;
     electrobunConfig.build.copy[`data/${!isDev ? "temp" : ""}system.json`] = `data/system.json`;
 
     const tempConfig = `import type { ElectrobunConfig } from "electrobun";\n\nexport default ${JSON.stringify(electrobunConfig, null, 2)} satisfies ElectrobunConfig`
 
     console.info("\nRewriting Electrobun config...");
-    await Bun.write(resolve(thisWorkSpace, "electrobun.config.ts"), tempConfig);
+    await Bun.write(resolve(thisWorkSpace, "temp_electrobun.config.ts"), tempConfig);
     console.info("Done.");
-    return electrobunConfigText
 }
