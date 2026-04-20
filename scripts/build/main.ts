4ffa453b0e6fb5fc74d1c3d3e308bdec0905c058
Kuumo Mai <maingocnhat2007@gmail.com>
2026-04-20 15:57:00 +0700

    Add custom config

--- scripts/build/main.ts
+++ scripts/build/main.ts
@@ -2,7 +2,7 @@
 export default async function Build_main_process() {
     console.log("Building main process for enviroment...");
     const start = new Date().getTime();
-    Bun.spawnSync(["bunx", "electrobun", "build", "--env=stable"], { stdout: "inherit", stderr: "inherit" })
+    Bun.spawnSync(["bunx", "electrobun", "build", "--env=stable", "--config=temp_electrobun.config.ts"], { stdout: "inherit", stderr: "inherit" })
 
     const end = new Date().getTime();
     console.log(`Built main process in ${((end - start) / 1000).toFixed(2)}s.`)
