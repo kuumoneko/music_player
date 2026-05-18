d17ab0b3c97657d9a82ba8cafef63f4276c1b635
Kuumo Mai <maingocnhat2007@gmail.com>
2026-05-18 22:19:42 +0700

    Fix: music pages not load

--- src/mainview/components/Music/index.tsx
+++ src/mainview/components/Music/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import Top from "@/mainview/components/Show/components/top.tsx";
 import List from "@/mainview/components/Show/components/list.tsx";
 import { Track } from "@/shared/types.ts";
@@ -12,24 +12,24 @@ export default function Music({
     type: "tracks" | "playlists" | "albums" | "artists";
     id: string;
 }) {
-    const tracks = useRef<Track[]>([]);
-    const data = useRef<any>({});
+    const [tracks, setTracks] = useState<Track[]>([]);
+    const [data, setData] = useState<any>({});
+
     const getdata = async () => {
         const result = await window.api.rpc.request.getMusicData({
             source: source as any,
             type: type as any,
             id: id,
         });
-
-        // @ts-ignore - tracks exists on Album and Playlist types returned by getMusicData
-        tracks.current = result.tracks || (type === "tracks" ? [result] : []);
-        data.current = result || {};
+        console.log(result);
+        setTracks(result.tracks || (type === "tracks" ? [result] : []));
+        setData(result ?? {});
     };
 
     useEffect(() => {
         let run = setInterval(() => {
             getdata();
-            if (tracks?.current?.length > 0) {
+            if (tracks?.length > 0) {
                 clearInterval(run);
                 run = setInterval(() => {
                     getdata();
@@ -45,15 +45,15 @@ export default function Music({
     return (
         <>
             <Top
-                name={data.current.name}
-                thumbnail={data.current.thumbnail}
-                duration={data.current.duration}
+                name={data.name}
+                thumbnail={data.thumbnail}
+                duration={data.duration}
                 source={source}
                 id={id}
                 mode={type}
-                list={tracks.current}
+                list={tracks}
             />
-            <List list={tracks.current} source={source} id={id} mode={type} />
+            <List list={tracks} source={source} id={id} mode={type} />
         </>
     );
 }
