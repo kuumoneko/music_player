e544302627151acb106aa4fb886d25b84617a96a
Kuumo Mai <maingocnhat2007@gmail.com>
2026-05-18 17:58:09 +0700

    Fix: parallelize HomeController with Promise.all

--- src/bun/controllers/home.ts
+++ src/bun/controllers/home.ts
@@ -1,4 +1,3 @@
-import { Artist } from "@/shared/types.ts";
 import Player from "../music/index.ts";
 
 export default async function HomeController(player: Player, pin: string[]) {
@@ -9,20 +8,14 @@ export default async function HomeController(player: Player, pin: string[]) {
         }
     })
     const artists = newPins.filter((item: any) => item.type.includes("artist"));
-    const pinArtists = [];
-    for (const item of artists) {
-        pinArtists.push(await player.youtube.fetch_artist(item.id))
-    }
+    const pinArtists = await Promise.all(artists.map((item: any) => player.youtube.fetch_artist(item.id)));
     const playlists = newPins.filter((item: any) => item.type.includes("playlist"));
-    const pinPlaylists = [];
-    for (const item of playlists) {
-        pinPlaylists.push(await player.youtube.fetch_playlist(item.id))
-    }
+    const pinPlaylists = await Promise.all(playlists.map((item: any) => player.youtube.fetch_playlist(item.id)));
     const tracks = newPins.filter((item: any) => item.type.includes("track"));
 
     const pinTracks = await player.youtube.fetch_track(tracks.map(item => item.id));
 
-    const ytb_new_tracks = await player.youtube.get_new_tracks(artists.filter((item: Artist) => item.source === "youtube").map((item: Artist) => item.id))
+    const ytb_new_tracks = await player.youtube.get_new_tracks(artists.filter((item: any) => item.source === "youtube").map((item: any) => item.id))
 
     return { artists: pinArtists, playlists: pinPlaylists, newTracks: ytb_new_tracks, tracks: pinTracks }
 }
