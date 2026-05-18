28c505758a84fa8ae2bf167cc00ac42bd1aa99cb
Kuumo Mai <maingocnhat2007@gmail.com>
2026-05-18 18:26:11 +0700

    FIx: wrong download item

--- src/mainview/components/Show/common/download.ts
+++ src/mainview/components/Show/common/download.ts
@@ -2,7 +2,7 @@ import { Track } from "@/shared/types.ts";
 
 export default async function download(item: Track) {
     const queue = await window.api.rpc.request.getUserData("downloadQueue");
-    queue.push(item.id)
+    queue.push(`${item.source}:track:${item.id}`)
     await window.api.rpc.request.setUserData({
         key: "downloadQueue",
         data: queue
