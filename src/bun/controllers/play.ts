2abb4f42c8c741395c97f2efafe4ef8bef8607ec
Kuumo Mai <maingocnhat2007@gmail.com>
2026-05-22 11:18:31 +0700

    Remove play controller

--- src/bun/controllers/play.ts
+++
@@ -1,8 +0,0 @@
-import { join } from "node:path";
-import { getUserData } from "../db";
-
-export default async function PlayController(folder: string): Promise<Response> {
-    const currentPlaying = getUserData("currentPlaying")
-    const file = Bun.file(join(folder, currentPlaying.id));
-    return new Response(file.stream());
-}
