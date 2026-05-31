51adc7b228e5ea5c805f6aff6e8b2abcce92c6e3
Kuumo Mai <maingocnhat2007@gmail.com>
2026-05-31 14:34:55 +0700

    Fix: wrong download queue items

--- src/mainview/components/Show/common/download.ts
+++ src/mainview/components/Show/common/download.ts
@@ -1,8 +1,8 @@
-import { Track } from "@/shared/types.ts";
+import { Artist, Playlist, Track } from "@/shared/types.ts";
 
-export default async function download(item: Track) {
+export default async function download(item: Track | Playlist | Artist, type: "tracks" | "playlists" | "artists") {
     const queue = await window.api.rpc.request.getUserData("downloadQueue");
-    queue.push(`${item.source}:track:${item.id}`)
+    queue.push(`${item.source}:${type}:${item.id}`)
     await window.api.rpc.request.setUserData({
         key: "downloadQueue",
         data: queue
