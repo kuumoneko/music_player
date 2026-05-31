6531a31213f441fe289a7e77f90b0cf324219837
Kuumo Mai <maingocnhat2007@gmail.com>
2026-05-31 14:34:18 +0700

    Fix artist not defined or null

--- src/shared/utils/formatArtist.ts
+++ src/shared/utils/formatArtist.ts
@@ -1,3 +1,4 @@
 export default function formatArtists(artists: { name: string, id: string }[]) {
+    if (artists === undefined || artists === null || artists.length === 0) return "";
     return artists.map(artist => artist.name).join(", ");
 }
